a20bc33ead721735532e906d0ee32362a06f4c0a
OlegEf <5tg6yy@gmail.com>
2020-04-11 22:36:34 +0300

    first com

---
+++ main.js
@@ -0,0 +1,86 @@
+// var fs = require('fs');
+// console.log(fs);
+
+// fs.writeFile('Hello.js', '2 str some cont', function(err){
+//  if (err) throw err;
+// });
+
+// console.log(fs.readFileSync('Hello.js', {encoding: 'utf-8'}));
+// console.log(''+fs.readFileSync('Hello.js'));
+// fs.mkdirSync('myFolder');
+// fs.renameSync('Hello.js', 'hjy7.js');
+// fs.renameSync('hj.js','Hello.js');
+//f6 move file in folder
+// fs.renameSync('Hello.js', 'myFolder/Hello.js');
+// fs.renameSync('hj.js','Hello.js');
+
+
+// 
+// 
+// TDK
+// end nodemone
+// 3.1.2
+// document.write("Hello World")
+
+// 2z
+	// function printHelloWorld() {
+	// 	var div1 = document.getElementByid("div1");
+	// 	div1.innerHTML="Hello, world";
+	// }
+// window.write("div1");
+// window.write("printHelloWorld");
+// window.write("printHelloWorld()");
+// window.alert("printHelloWorld");
+
+// 3.1.4 a
+// console.log("Hello world" ); //undefined
+
+// 3.1.6 a
+// window.alert("Hello World");
+// document.write("Hello World\n string2")
+// document.write("Hello World\n string2")
+// ----3.1.7 not work
+
+// if  (window.сопfігm("Нажмите  одну  из  кнопок"))  {
+// 	аlегt("Нажата  кнопка  OK");
+// }
+// else  {
+// 	alert("Нажата  кнопка  Cancel");
+// }
+// if  (сопfігm("Нажмите  одну  из  кнопок"))  {
+// 	аlегt("Нажата  кнопка  OK");
+// }
+// else  {
+// 	alert("Нажата  кнопка  Cancel");
+// }
+
+// ----3.10 not work
+    // $(function() {
+    //     $("#div1").text("Hello, world");
+    // });
+
+// 3.2 
+// var x=12;
+// var strName=12;
+// var _name=12;
+// var window=12;
+// document.write(strName);
+// document.write(x);
+
+// 3.2.2
+// var x, y, z=12;
+// var x=y=z=12;
+// var x=y=z=12;
+// document.write(x,y,z);
+
+// 3.2.3
+// console.log(typeof str ); //undefined
+// console.log(typeof boolean ); //undefined
+// console.log(typeof 1 ); //number
+// console.log(typeof 'string' ); //string
+// console.log(typeof true ); //true
+// console.log(typeof false ); //true
+// console.log(typeof function() {} ); //function
+// console.log(typeof [1,2,3] ); //object
+
+// 3.2.4
